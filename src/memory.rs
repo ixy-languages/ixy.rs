389af454061afe642a2dbdc15de83e057b93d1b5
Boris-Chengbiao Zhou <bobo1239@web.de>
2019-03-26 03:06:26 +0100

    Fix (clippy) warnings

--- src/memory.rs
+++ src/memory.rs
@@ -8,7 +8,7 @@ use std::ops::{Deref, DerefMut};
 use std::os::unix::prelude::AsRawFd;
 use std::process;
 use std::rc::Rc;
-use std::sync::atomic::{AtomicUsize, Ordering, ATOMIC_USIZE_INIT};
+use std::sync::atomic::{AtomicUsize, Ordering};
 use std::{ptr, slice};
 
 use crate::IxyDevice;
@@ -17,7 +17,7 @@ use libc;
 const HUGE_PAGE_BITS: u32 = 21;
 const HUGE_PAGE_SIZE: usize = 1 << HUGE_PAGE_BITS;
 
-static HUGEPAGE_ID: AtomicUsize = ATOMIC_USIZE_INIT;
+static HUGEPAGE_ID: AtomicUsize = AtomicUsize::new(0);
 
 pub struct Dma<T> {
     pub virt: *mut T,
@@ -27,7 +27,7 @@ pub struct Dma<T> {
 const VFIO_DMA_MAP_FLAG_READ: u32 = 1;
 const VFIO_DMA_MAP_FLAG_WRITE: u32 = 2;
 const VFIO_IOMMU_MAP_DMA: u64 = 15217;
-const MAP_HUGE_2MB: i32 = 0x54000000; // 21 << 26
+const MAP_HUGE_2MB: i32 = 0x5400_0000; // 21 << 26
 
 // struct vfio_iommu_type1_dma_map, grabbed from linux/vfio.h
 #[allow(non_camel_case_types)]
