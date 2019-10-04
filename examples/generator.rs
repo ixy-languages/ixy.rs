2a82db52bcb4843f6488072e378e8cd65f6f88e2
Boris-Chengbiao Zhou <bobo1239@web.de>
2019-10-05 00:23:10 +0200

    Fix IPv4 checksum calculation and wait until all packets are queued
    The previous checksum calculation was incorrect which caused packets
    to be filtered out by Virtio bridges.
    Also wait until all packets are queued to match the upstream pktgen
    example application.
    Finally the source MAC is changed so it isn't a multicast address
    anymore as that is also filtered out by Virtio bridges.

--- examples/generator.rs
+++ examples/generator.rs
@@ -3,6 +3,7 @@ use std::env;
 use std::process;
 use std::time::Instant;
 
+use byteorder::{ByteOrder, LittleEndian};
 use ixy::memory::{alloc_pkt_batch, Mempool, Packet};
 use ixy::*;
 
@@ -32,7 +33,7 @@ pub fn main() {
     #[rustfmt::skip]
     let pkt_data = [
         0x01, 0x02, 0x03, 0x04, 0x05, 0x06,         // dst MAC
-        0x11, 0x12, 0x13, 0x14, 0x15, 0x16,         // src MAC
+        0x10, 0x10, 0x10, 0x10, 0x10, 0x10,         // src MAC
         0x08, 0x00,                                 // ether type: IPv4
         0x45, 0x00,                                 // Version, IHL, TOS
         ((PACKET_SIZE - 14) >> 8) as u8,            // ip len excluding ethernet, high byte
@@ -61,7 +62,8 @@ pub fn main() {
             for (i, data) in pkt_data.iter().enumerate() {
                 p[i] = *data;
             }
-            let checksum = calc_ip_checksum(p, 14, 20);
+            let checksum = calc_ipv4_checksum(&p[14..14 + 20]);
+            // Calculated checksum is little-endian; checksum field is big-endian
             p[24] = (checksum >> 8) as u8;
             p[25] = (checksum & 0xff) as u8;
         }
@@ -86,11 +88,11 @@ pub fn main() {
 
         // update sequence number of all packets (and checksum if necessary)
         for p in buffer.iter_mut() {
-            p[PACKET_SIZE - 4] = seq_num;
-            seq_num = (seq_num % std::u8::MAX) + 1;
+            LittleEndian::write_u32(&mut p[(PACKET_SIZE - 4)..], seq_num);
+            seq_num = seq_num.wrapping_add(1);
         }
 
-        dev.tx_batch(0, &mut buffer);
+        dev.tx_batch_busy_wait(0, &mut buffer);
 
         // don't poll the time unnecessarily
         if counter & 0xfff == 0 {
@@ -110,14 +112,35 @@ pub fn main() {
     }
 }
 
-// calculate IP/TCP/UDP checksum
-fn calc_ip_checksum(packet: &mut Packet, offset: usize, len: usize) -> u16 {
+/// Calculates IPv4 header checksum
+fn calc_ipv4_checksum(ipv4_header: &[u8]) -> u16 {
+    assert!(ipv4_header.len() % 2 == 0);
     let mut checksum = 0;
-    for i in 0..len / 2 {
-        checksum += ((u32::from(packet[i + offset])) << 8) + u32::from(packet[i + offset + 1]);
+    for i in 0..ipv4_header.len() / 2 {
+        if i == 5 {
+            // Assume checksum field is set to 0
+            continue;
+        }
+        checksum += (u32::from(ipv4_header[i * 2]) << 8) + u32::from(ipv4_header[i * 2 + 1]);
         if checksum > 0xffff {
-            checksum = (checksum & 0xfff) + 1;
+            checksum = (checksum & 0xffff) + 1;
         }
     }
     !(checksum as u16)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ipv4_checksum() {
+        // Test case from the Wikipedia article "IPv4 header checksum"
+        assert_eq!(
+            calc_ipv4_checksum(
+                b"\x45\x00\x00\x73\x00\x00\x40\x00\x40\x11\xb8\x61\xc0\xa8\x00\x01\xc0\xa8\x00\xc7"
+            ),
+            0xb861
+        );
+    }
 }
