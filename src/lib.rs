12376e915ed11bb780d0bf3d487a3d2c6d1f06df
Simon Ellmann <simon.ellmann@tum.de>
2019-10-14 19:35:41 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -223,7 +223,7 @@ pub fn ixy_init(
 
     if vendor_id == 0x1af4 && device_id == 0x1000 {
         // `device_id == 0x1041` would be for non-transitional devices which we don't support atm
-        let device = VirtioDevice::init(pci_addr, rx_queues, tx_queues)?;
+        let device = VirtioDevice::init(pci_addr, rx_queues, tx_queues, interrupt_timeout)?;
         Ok(Box::new(device))
     } else {
         // let's give it a try with ixgbe
