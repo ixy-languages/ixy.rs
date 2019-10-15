be0ae0ec76c664d68c0ea1f7ca075823fa311d98
Boris-Chengbiao Zhou <bobo1239@web.de>
2019-10-15 10:24:08 +0200

    Rename Queue trait to Ring

--- src/ixgbe.rs
+++ src/ixgbe.rs
@@ -183,7 +183,10 @@ impl IxyDevice for IxgbeDevice {
         let mut received_packets = 0;
 
         {
-            let queue = self.rx_queues.get_mut(queue_id as usize).expect("invalid rx queue id");
+            let queue = self
+                .rx_queues
+                .get_mut(queue_id as usize)
+                .expect("invalid rx queue id");
 
             rx_index = queue.rx_index;
             last_rx_index = queue.rx_index;
@@ -257,7 +260,10 @@ impl IxyDevice for IxgbeDevice {
         let mut sent = 0;
 
         {
-            let mut queue = self.tx_queues.get_mut(queue_id as usize).expect("invalid tx queue id");
+            let mut queue = self
+                .tx_queues
+                .get_mut(queue_id as usize)
+                .expect("invalid tx queue id");
 
             let mut cur_index = queue.tx_index;
             let clean_index = clean_tx_queue(&mut queue);
